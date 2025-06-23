315444b58a55a8a3cf47f3397ca239928a7d8495
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-24 02:11:38 +0530

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DoctorsModule } from './doctors/doctors.module';
+import { User } from './users/user.entity';
+import { Doctor } from './doctors/doctor.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Doctor],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    DoctorsModule,
+  ],
 })
 export class AppModule {}
