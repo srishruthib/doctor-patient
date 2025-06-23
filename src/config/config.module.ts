9cd090debbdb721827825aeefae81b60195331a6
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-24 02:53:57 +0530

    Update config.module.ts

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,1 +1,13 @@
-// Configuration module for env variables
+// Configuration module for env variablesimport { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
+})
+export class ConfigModule {}
