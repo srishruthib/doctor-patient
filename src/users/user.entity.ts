4007f2784f43bca9aa0811241097be9fd75db925
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-24 02:31:53 +0530

    Add files via upload

---
+++ src/users/user.entity.ts
@@ -0,0 +1,23 @@
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  password: string;
+
+  @Column()
+  provider: 'local' | 'google';
+
+  @Column()
+  role: 'doctor' | 'patient';
+}
