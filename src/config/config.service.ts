453b9153744a8e87c1b5731de525f17946ec31c0
ADITYA SINGH <pglapgla123@gmail.com>
2025-06-24 02:54:20 +0530

    Update config.service.ts

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,1 +1,15 @@
-// Config service
+// Config serviceimport { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ConfigService {
+  constructor(private config: NestConfigService) {}
+
+  get(key: string): string {
+    return this.config.get<string>(key);
+  }
+
+  getNumber(key: string): number {
+    return Number(this.config.get<string>(key));
+  }
+}
